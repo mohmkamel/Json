75b581d92079960595b569992e1d86b0158aa74b
mohmkamel <50558165+mohmkamel@users.noreply.github.com>
2022-05-11 16:54:06 +0200

    Add files via upload
    this query take data from P_sheet table and Pitems table to insert it in PurInv_items table, depend on data in specific attribute and divide the quantity according to the result.
    using CASE WHEN and THEN and FLOOR clauses to present the described result

---
+++ PurInv_ins.sql
@@ -0,0 +1,35 @@
+use newphar
+--select PID , P_Name , UOM , QTY from Pitems
+--where --Qty is not null
+--UOM is null
+
+--select * from PItems PIT
+--inner join P_sheet PS
+--on PIT.P_name = PS.name 
+--where --PIT.qty is not null
+
+--select count(*) from Pitems
+
+
+
+insert into PurInv_items (PID , itm_Qty , Expr_date , Sell_pric)
+select P.PID , case when E_date_1 is null then P.qty
+				else  floor (P.qty/2) end  as qty, 
+				datefromparts(cast (concat (20,left (E_date,2)) as char(10)) ,
+					case when len(E_date)=4 then cast (concat(0,trim (right(E_date,1))) as char(10))
+					else cast (trim(right(E_date,2)) as char(10)) end, 1) as E_date, 
+				U__price from Pitems P
+				inner join P_sheet PS
+				on P.P_name = PS.name
+where P.qty is not null
+
+insert into PurInv_items ( PID , Itm_Qty , Expr_date , Sell_Pric)
+select P.PID ,  P.qty - floor (P.qty/2)  as qty , 
+	datefromparts(cast (concat (20,left (E_date_1,2)) as char(10)) ,
+					case when len(E_date_1)=4 then cast (concat(0,trim (right(E_date_1,1))) as char(10))
+					else cast (trim(right(E_date_1,2)) as char(10)) end, 1) as E_date_1, 
+				U__price from Pitems P
+				inner join P_sheet PS
+				on P.P_name = PS.name
+where E_date_1 is not null
+
